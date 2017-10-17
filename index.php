6c38c5f6172d2ce47f9e7b3073676c057b3faa1d
Cyrht <cyr.huet@gmail.com>
2017-10-17 15:00:02 +0200

    adding main.ph

--- index.php
+++ index.php
@@ -24,6 +24,8 @@
 
 	<?php include ("includes/header.php"); ?>
 
+	<?php include ("includes/main.php"); ?>
+
 	<?php include ("includes/footer.php"); ?>
 
 </body>
--- styles.php
+++ styles.php
@@ -18,10 +18,129 @@
 </head>
 
 <body>
+
 	<!--Header réalisé par Emeline /include -->
 	<!--Nav bar réalisée par Emeline /include -->
 	<?php include ("includes/header.php"); ?>
 	 
+<div class="container">
+	
+	<div class="article">
+	
+		<p class="introduction"> Le jazz ne cesse de fusionner avec d'autres styles de musique tout en restant ce qu'il est. <br />
+		Des premiers work songs aux métissages multiethniques, des gospels aux expériences électroniques, du blues aux rapprochements avec le hip-hop, nous abordons ici, en suivant la chronologie, les grandes phases de l'histoire du jazz : spirituals, gospel, ragtime, jazz hot, swing, bop, cool, hard bop, free, fusion, etc. L'évolution du jazz a été conditionnée par la place faite aux Africain-Americain dans la société américaine, infléchie par le poids de l'histoire, par le voisinage d'autres musiques, par l'apparition de nouvelles techniques d'enregistrement et de nouveaux instruments...</p> 
+	</div>
+
+		<div class="style">
+	<img src="https://upload.wikimedia.org/wikipedia/en/9/95/Ragtime.jpg" class="picture1">
+
+	<h2>Ragtime</h2>
+
+	<p>Le ragtime est un genre musical d'origine américaine extrêmement populaire dans les années 1900-1918. On trouve dans le Cakewalk et les musiques de salon une forme primitive de ragtime, mais on situe généralement l'émergence du ragtime classique en 1897, année où furent publiées plusieurs compositions importantes de ragtime (communément appelées rags). Du point de vue musical, le ragtime se caractérise principalement par l'introduction de syncope dans la mélodie : les notes sont parfois jouées entre les temps principaux du rythme, créant un effet de décalage.<br />
+	Musiciens : Scott Joplin - Joseph Lamb - James Scott.</p>
+		</div>
+
+		<div class="style">
+	<img src="https://images-na.ssl-images-amazon.com/images/I/51YwfHkYoOL._SS500.jpg">
+
+	<h2>New Orleans</h2>
+
+	<p>Le jazz Nouvelle-Orléans (New Orléans Jazz) est un courant musical du jazz qui s'est principalement développé dans les années 1910 et 1920 à la Nouvelle-Orléans — c'est là qu'est né le jazz dans les années 1910 avant d'émigrer vers Chicago — avant de s'éclipser face au middle jazz ou swing vers la fin des années 1920. L'une des caractéristiques de ce courant est le style « tailgate », consistant pour le trombone à accompagner le jeu de la trompette par un contrepoint rythmique, ponctué d'effets de glissandi. Le nom « tailgate » vient du fait que les orchestres défilaient sur un chariot traîné par des chevaux.<br />
+	Musiciens : Louis Armstrong - Jelly Roll Morton - King Oliver. </p>
+		</div>
+
+		<div class="style">
+	<img src="https://pbs.twimg.com/profile_images/570946641483718656/6pj4nFip.jpeg">
+
+	<h2>Swing</h2>
+
+	<p>Le middle jazz ou swing (jazz du « milieu ») est un courant musical du jazz qui prend son essor du milieu des années 20. Il est surtout caractérisé par le développement des grands orchestres et big bands et du swing. Par extension, ce courant est également appelé «ère des big bands», «époque du swing» ou tout simplement «swing». Un élément du swing est le développement des sections de cuivres, et particulièrement des saxophones. Chaque orchestre acquiert une identité et une couleur qui lui est propre, surtout grâce au travail des arrangeurs et des chefs d’orchestre qui adaptent les compositions à la personnalité de l’orchestre, voire de chaque musicien.<br /> 
+	Musiciens : Duke Ellington - Count Basie - Benny Goodman.</p>
+		</div>
+
+
+		<div class="style">
+	<img src="https://images-na.ssl-images-amazon.com/images/I/51p2OYIHfUL._SX258_BO1,204,203,200_.jpg">
+
+	<h2>Manouche</h2>
+
+	<p>Swing inspiré du jeu du guitariste manouche Django Reinhardt (1910-1953). Ce style de jazz se fonde sur le répertoire enregistré par Django Reinhardt c.-à-d. ses propres compositions telles Minor Swing et Nuages et des standards de jazz des années 20 et 30 tels qu' All of Me, After You've Gone ou I Can't Give You Anything But Love. Ce style s'illustre par l'absence de batterie et de cuivres.  Il s'identifie par une rythmique assumée par une ou plusieurs guitares qui accentuent les 2e et 4e temps de la mesure de 4/4. La contrebasse jouera dans un style de swing traditionnel et accentuera les 1er et 3e temps. Le premier soliste, habituellement guitariste se verra donner la réplique soit par un violoniste, un clarinettiste ou un accordéoniste.<br />
+	Musiciens : Django Reinhart - Bireli Lagrene.</p>
+		</div>
+
+
+		<div class="style">
+	<img src="https://static.fnac-static.com/multimedia/Images/FR/NR/a8/2f/88/8925096/1540-1/tsp20170706110233/50-s-Be-Bop-Jazz.jpg">
+
+	<h2>BE BOP</h2>
+
+	<p>Les premiers enregistrements datent de 1945. Ces groupes de jazz sont souvent composés d'une poignée de musiciens qui recherchaient après leurs obligations contractuelles dans de grands orchestres à s'affranchir de la discipline des big bands. Ces formations plus réduites laissent plus de liberté dans l'interprétation et plus d'opportunités de jouer des solos. Le be-bop se singularise desautres styles de jazz par un tempo souvent très rapide, des phrasés pêchus et des grilles harmoniques très fournies, les accords changent toutes les mesures voire très souvent plusieurs fois par mesure. Ce style demande donc une grande maîtrise technique de son instrument ainsi qu'une bonne oreille et une connaissance assez approfondie de la théorie musicale.<br />
+	Musiciens : Charlie Parker - Dizzy Gillespie - Thelonius Monk </p>
+		</div>
+
+	<div class="style">
+	<img src="https://e.snmc.io/lk/l/s/877357c04cedf9ddc9be9893cc3cdba6/3361551.jpg">
+
+	<h2>COOL</h2>
+
+	<p>Le cool jazz né en 1949 sous la houlette de Miles Davis qui cherche à briser la spirale frénétique du be-bop vers plus de virtuosité. L'album The Birth of Cool en 1949 marque un retour à des rythmes plus apaisés et des compositions plus simples. Ce caractère plus abordable lui permettra de connaitre un succès rapide, en particulier auprès des musiciens de la côte ouest des États-Unis avant de se prêter à de nombreux métissages (notamment avec la musique brésilienne pour créer la bossa-nova ). Le cool jazz est un jazz libre, rempli de silence et d'intensité. Il contient tout le bebop dans ses phrasés, dans l'attitude des musiciens et c'est aussi une musique qui s'écoute facilement, que l'on peut apprécier dans tous les instants.<br />
+	Musiciens : Miles Davis - Chet Baker - Dave Brubeck  </p>
+		</div>
+
+	<div class="style">
+	<img src="https://img.discogs.com/1wnunBSNANGbn8qlP8ZxQ3qqpIU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1878084-1256141942.jpeg.jpg">
+
+	<h2>FREE</h2>
+
+	<p>Le free jazz est un style de musique qui se développe dans les années 1950-1960. John Coltrane est un des musiciens qui, dans ses dernières œuvres, réalise quelques-uns des meilleurs morceaux de free jazz. Habituellement, cette musique est jouée par de petits groupes de musiciens. Dans l'esprit populaire, le free jazz est lourd, agressif et dissonant. Beaucoup de critiques pensent que l'abandon des éléments familiers du jazz est dû à un manque de technique de la part des musiciens. Ce point de vue est devenu marginal et cette musique a pu se construire une forte tradition. Le free jazz utilise les bases du jazz mais avec une composition moins structurée que les styles précédents. L'improvisation, par exemple, y tient une grande place, le free jazz étant d'ailleurs un des principaux « inspirateurs » du genre improvisation libre.<br />
+	Musiciens : John Coltrane - Ornette Coleman - Cecil Taylor - John Zorn </p>
+		</div>
+	
+	<div class="style">
+	<img src="https://i.pinimg.com/736x/42/10/7e/42107e57bab434d95f251a163e62259e--hard-bop-design-typo.jpg">
+
+	<h2>HARD BOP</h2>
+
+	<p>Cette musique début en 1955-1960 et est un retour aux sources de la musique, à l'Afrique et, en même temps, une réaction agressive (musicalement parlant) au cool jazz (d'où le terme « hard ») surtout dominé par les blancs. Même si la plupart des acteurs de ce courant on fait leur apprentissage dans le style bebop (d'où le terme « bop »), ce genre musical incorpore les influences du rhythm and blues, du blues et du gospel. Les morceaux de hard bop ont généralement un tempo plus lent que le bebop, et si le hard bop en reprend les innovations harmoniques, la part du rythme y est nettement plus marquée, sans doute en raison de la contribution majeure des batteurs Max Roach et Art Blakey.<br />
+	Musiciens : Art Blakey - Max Roach - Sonny Rollins</p>
+		</div>
+
+	<div class="style">
+	<img src="http://cdn.musicjazzfree.com/domains/www.musicjazzfree.com/genre/jazz-fusion/jazz-fusion-2.jpg">
+
+	<h2>FUSION</h2>
+
+	<p>Le jazz-rock, appelé parfois jazz fusion, est un style musical né à la fin des années 1960. Ce style est lancé par Miles Davis, lorsque celui-ci prend ses distances avec le be-bop pour se tourner vers la musique amplifiée, mariant les influences du jazz, du rock, du rhythm and blues, du soul music, du funk et parfois de la musique classique. Ses albums « Bitches Brew » et « In a Silent Way » (1969) sont considérés comme l'acte de naissance de ce style. Un autre album incontournable du style est « The Inner Mounting Flame» (1971) du Mahavishnu Orchestra de John McLaughlin.<br />
+	Musiciens : Miles Davis - John McLaughlin - Weather Report - Uzeb. </p>
+		</div>
+
+	<div class="style">
+	<img src="http://www.parisjazzcorner.com/pochs_g/092659.jpg">
+
+	<h2>LATIN</h2>
+
+	<p>Le jazz latin est le mariage des rythmes issus de l'Afrique et de l'Amérique latine et du jazz américain. Les deux catégories principales sont l'afro-cubain et le brésilien. L'afro-cubain inclut salsa, merengue, songo, son, mambo, bolero, charanga et cha cha cha. Le brésilien inclut le bossa-nova et la samba. L'afro-cubain prends ses racines à la fin des années quarante quand Dizzy Gillespie et Stan Kenton combinent les sections rhythmiques et structures de l'afro-cubain, tels que Machito et ses afro-cubains avec des instruments de jazz et l'improvisation. Poncho Sanchez lance Conga Blue un classique du genre.<br />
+	Musiciens : Poncho Sanchez - Machito - Antonio Carlos Jobim.</p>
+		</div>
+
+	<div class="style">
+	<img src="https://images-na.ssl-images-amazon.com/images/I/51KNFH6766L.jpg">
+
+	<h2>ACID</h2>
+
+	<p>L'acid jazz (également appelé groove jazz) est un style musical qui combine des influences de jazz avec des éléments issus de la soul music, du funk, du disco et du hip-hop. L'acid jazz prend son essor dans les années 1980 et 1990, d'abord à Londres grâce à des labels comme Talking Loud, puis dans le monde entier. Une des idées du genre était de retourner le jazz à ses racines, c'est à dire les salles de danse. Ceci explique le style plus soul, groove ainsi que des mélodies et un son plus accrocheur.<br />
+	Musiciens : Guru.</p>S
+		</div>
+
+
+
+
+	
+	</div>
+</div>
+
+
+
 
 </body>
 </html>
