495cf1aa2649db94da7aa82924f5a560d8407953
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2017-10-17 16:42:20 +0200

    Création des fichiers newsfeed.php et newsfeed.css

---
+++ includes/newsfeed.php
@@ -0,0 +1,36 @@
+<section class="newsfeed">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-lg-12 col-lg-offset-9">
+				
+				<h3>Prochains concerts</h3>
+
+				<div class="news">
+					<img src="assets/pictures/wallaceroneyquintet.jpg" alt="concert">
+					<h4>Wallace Roney Quintet</h4>
+					<p>New Morning</p>
+					<p>17 octobre 2017 à 20h</p>
+					<a href="http://www.digitick.com/wallace-roney-quintet-concert-new-morning-paris-17-octobre-2017-css4-digitick-pg101-ri4772075.html">Achetez vos places</a>
+				</div>
+				
+				<div class="news">
+					<img src="assets/pictures/johnbutcher.jpg" alt="concert">
+					<h4>John Butcher, Matthew Shipp et Thomas Lehn</h4>
+					<p>Les Instants Chavirés</p>
+					<p>17 octobre 2017 à 20h30</p>
+					<a href="https://www.yesgolive.com/instants-chavires/john-butcher-matthew-shipp-thomas-lehn">Achetez vos places</a>
+				</div>
+
+				<div class="news">
+					<img src="assets/pictures/sidneybechet.jpg" alt="concert">
+					<h4>Hommage À Sidney Bechet</h4>
+					<p>Le Méridien Étoile</p>
+					<p>19 octobre 2017 à 21h30</p>
+					<a href="http://www.fnacspectacles.com/place-spectacle/manifestation/Jazz-HOMMAGE-A-SIDNEY-BECHET-JC19O.htm?tduid=446663d9dc856c649bd511f351ec5a41">Achetez vos places</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
+</section>
