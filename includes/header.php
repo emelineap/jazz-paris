d2f7d9421413bffcb5b27c68117d432113cdc671
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2017-10-17 21:54:50 +0200

    Intégration du newsfeed, fix bug main.css

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,44 @@
-<header class="container">
-	<div class="row">
-		<div class="in-header col-xs-12 col-lg-12">
-			<a href="index.html"><img src="assets/pictures/Glenn_Branca-15_logo.jpg" alt="banner jazz paris"></a>
+<?php
+      // On recupere l'URL de la page pour ensuite affecter class = "active" aux liens de nav
+$page = $_SERVER['REQUEST_URI'];
+$page = str_replace("/wcs_projets/jazz-paris/", "",$page);
 
-			<nav id="nav" class="row">
-				<div class="col-xs-2 col-lg-2">
+function ispageactive($currentpage)
+{
+      	//global permet de récupérer une variable qu'on définit en dehors de la fonction
+	global $page;
+	if($currentpage == $page)
+	{
+		return 'active';
+	}
+}
+?>
+
+<header class="container">
+	<div class="in-header">
+		<a href="index.html"><img src="assets/pictures/Glenn_Branca-15_logo.jpg" alt="banner jazz paris"></a>
+
+		<nav>
+			<ul class="header-nav row">
+				<li class="col-xs-2 col-lg-2">
 					<h1><a href="index.php">JAZZ PARIS</a></h1>
-				</div>
-				<ul>
-					<li class="active"><a class="col-offset-2 col-xs-2 col-lg-2" href="index.php" id="index">Accueil</a></li>
-					<li class="active"><a class="col-offset-4 col-xs-2 col-lg-2" href="origins.php" id="origins">Les origines</a></li>
-					<li class="active"><a class="col-offset-6 col-xs-2 col-lg-2" href="styles.php" id="styles">Les styles</a></li>
-					<li class="active"><a class="col-offset-8 col-xs-2 col-lg-2" href="today.php" id="today">Aujourd'hui</a></li>
-					<li class="active"><a class="col-offset-10 col-xs-2 col-lg-2" href="about.php" id="about">Contact</a></li>
-				</ul>
-			</nav>
-		</div>	
+				</li>
+				<li class="col-offset-2 col-xs-2 col-lg-2 <?php echo ispageactive('index.php') ?>">
+					<a href="index.php">Accueil</a>
+				</li>
+				<li class="col-offset-2 col-xs-2 col-lg-2 <?php echo ispageactive('origins.php') ?>">
+					<a href="origins.php">Les origines</a>
+				</li>
+				<li class="col-offset-2 col-xs-2 col-lg-2 <?php echo ispageactive('styles.php') ?>">
+					<a href="styles.php">Les styles</a>
+				</li>
+				<li class="col-offset-2 col-xs-2 col-lg-2 <?php echo ispageactive('today.php') ?>">
+					<a href="today.php">Aujourd'hui</a>
+				</li>
+				<li class="col-offset-2 col-xs-2 col-lg-2 <?php echo ispageactive('about.php') ?>">
+					<a href="about.php">Contact</a>
+				</li>
+			</ul>
+		</nav>
 	</div>	
 </header>
--- includes/main.php
+++ includes/main.php
@@ -8,7 +8,6 @@
 				<!-- CATÉGORIES -->
 
 				<div class="col-xs-12 col-lg-9">
-
 					<div class="left">
 
 						<a href="/origines.php"><img class="picture_categories" src="assets/pictures/colinstetson-4.jpg" width="900" height="400" alt= "photo de Colin Stetson"></a>
@@ -29,35 +28,35 @@
 				</div>
 
 				<!-- FIL D'ACTU -->
-				<div class="right"> 
-					<aside class="newsfeed">
-							<div class="col-xs-12 col-lg-3 col-lg-offset-9">
+				<div class="col-xs-12 col-lg-3">
+					<div class="right"> 
+						<aside class="newsfeed">
 
-								<h5>Prochains concerts</h5>
+							<h5>PROCHAINS CONCERTS</h5>
 
-								<div class="news">
-									<img src="assets/pictures/wallaceroneyquintet.jpg" alt="concert">
-									<h6>Wallace Roney Quintet</h6>
-									<p>New Morning</p>
-									<p>17 octobre 2017 à 20h</p>
-									<a href="http://www.digitick.com/wallace-roney-quintet-concert-new-morning-paris-17-octobre-2017-css4-digitick-pg101-ri4772075.html">Achetez vos places</a>
-								</div>
+							<div class="news">
+								<img src="assets/pictures/wallaceroneyquintet.jpg" alt="concert">
+								<h6>Wallace Roney Quintet</h6>
+								<p>New Morning</p>
+								<p>17 octobre 2017 à 20h</p>
+								<a href="http://www.digitick.com/wallace-roney-quintet-concert-new-morning-paris-17-octobre-2017-css4-digitick-pg101-ri4772075.html">Achetez vos places</a>
+							</div>
 
-								<div class="news">
-									<img src="assets/pictures/johnbutcher.jpg" alt="concert">
-									<h6>John Butcher, Matthew Shipp et Thomas Lehn</h6>
-									<p>Les Instants Chavirés</p>
-									<p>17 octobre 2017 à 20h30</p>
-									<a href="https://www.yesgolive.com/instants-chavires/john-butcher-matthew-shipp-thomas-lehn">Achetez vos places</a>
-								</div>
+							<div class="news">
+								<img src="assets/pictures/johnbutcher.jpg" alt="concert">
+								<h6>John Butcher, Matthew Shipp et Thomas Lehn</h6>
+								<p>Les Instants Chavirés</p>
+								<p>17 octobre 2017 à 20h30</p>
+								<a href="https://www.yesgolive.com/instants-chavires/john-butcher-matthew-shipp-thomas-lehn">Achetez vos places</a>
+							</div>
 
-								<div class="news">
-									<img src="assets/pictures/sidneybechet.jpg" alt="concert">
-									<h6>Hommage À Sidney Bechet</h6>
-									<p>Le Méridien Étoile</p>
-									<p>19 octobre 2017 à 21h30</p>
-									<a href="http://www.fnacspectacles.com/place-spectacle/manifestation/Jazz-HOMMAGE-A-SIDNEY-BECHET-JC19O.htm?tduid=446663d9dc856c649bd511f351ec5a41">Achetez vos places</a>
-								</div>
+							<div class="news">
+								<img src="assets/pictures/sidneybechet.jpg" alt="concert">
+								<h6>Hommage À Sidney Bechet</h6>
+								<p>Le Méridien Étoile</p>
+								<p>19 octobre 2017 à 21h30</p>
+								<a href="http://www.fnacspectacles.com/place-spectacle/manifestation/Jazz-HOMMAGE-A-SIDNEY-BECHET-JC19O.htm?tduid=446663d9dc856c649bd511f351ec5a41">Achetez vos places</a>
+							</div>
 						</div>
 					</aside>
 
