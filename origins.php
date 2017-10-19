60718fdd060e9681819f8efe7f95482e2a7ca732
Elisa <elisa.laristan@gmail.com>
2017-10-19 16:08:32 +0200

    retravail page contact et css origins

--- origins.php
+++ origins.php
@@ -12,8 +12,8 @@
 	<!-- CUSTOM CSS -->
 	
 	<link rel="stylesheet" type ="text/css" href="assets/css/header.css" />
-	<link rel="stylesheet" type ="text/css" href="assets/css/origins.css" />
 	<link rel="stylesheet" type ="text/css" href="assets/css/footer.css" />
+	<link rel="stylesheet" type ="text/css" href="assets/css/origins.css" />
 
 </head>
 
@@ -27,29 +27,26 @@
 		
 		<div class="article">
 			
-			<p class="introduction"> Les origines du jazz en France <br />
+			<p class="introduction">
 				Pour nombre d'historiens ou musicologues avertis, le jazz est une création américaine née de la rencontre fortuite de traditions ancestrales portées par les esclaves africains, et d'harmonies classiques séculaires préservées par les colons européens pendant des siècles. Ce choc culturel entre Blancs et Noirs sera la source d'un genre musical révolutionnaire et militant !
 			</p> 
 		</div>
 
 		<div class="style">
-			<img src="http://scd.musique.rfi.fr/sites/default/files/styles/large_normal_982x552/public/thumbnails/image/jazz-orchestra-1921-ok_0_0_5.jpg?itok=DuS7F5jD&timestamp=1469543409" class="img1">
+			<img src="http://scd.musique.rfi.fr/sites/default/files/styles/large_normal_982x552/public/thumbnails/image/jazz-orchestra-1921-ok_0_0_5.jpg?itok=DuS7F5jD&timestamp=1469543409" class="img1" alt="jazz">
 
 			<h2>Origines</h2>
 
-			<p>Bien que le jazz soit aujourd'hui unanimement reconnu comme une forme d'expression universelle, son développement international fut souvent semé d'embûches. Il y a un siècle, aux Etats-Unis, le contexte politique et social bâillonnait les manifestations d'une culture «nègre» jugée primitive et sans valeur. Au même moment, la France coloniale portait un regard paternaliste et condescendant sur les populations noires d'Afrique. Ainsi, lorsque les premiers échos d'un jazz américain balbutiant parvinrent aux oreilles de musiciens européens aguerris, les réactions furent dubitatives, amusées, parfois dédaigneuses et toujours dictées par une compréhension primaire d'un art dit «exotique». Il est donc difficile de dater précisément la naissance du jazz en France. D'autant plus difficile d'en dessiner les contours... Le pianiste, chef d'orchestre et pédagogue français, Laurent Cugny, s'est lancé dans une étude approfondie du paysage musical européen en éditant le 1er Tome d'une série de 4 ouvrages, intitulée «Une histoire du Jazz en France» (Outre Mesure Editions), qui narre dans le détail l'évolution progressive des mentalités face à l'émergence d'une culture métisse, imprévisible et libre.
-			</p>
-		</div>
-		<div>
-			
-			<p>Avant 1808, la traite des esclaves avait déjà provoqué l'arrivée aux États-Unis d'un million d'Africains subsahariens, principalement en provenance d'Afrique de l'Ouest et du bassin du fleuve Congo. Ceux-ci ont importé leurs traditions musicales : la musique africaine utilisait une seule mélodie, des rythmes contre-métriques, et une structure en appel et réponse  (exemple : work song). Elle avait essentiellement un but fonctionnel, pour accompagner le travail (chant de travail) ou les rites funéraires. Des rassemblements d'esclaves donnaient lieu à de grandes fêtes urbaines où se mêlaient danses africaines et percussions. Jusqu'en 1843, une fête était organisée chaque dimanche à Congo Square, à la Nouvelle-Orléans, et d'autres rassemblements avaient lieu à la même époque dans le Sud des États-Unis.</br>
+			<p>Bien que le jazz soit aujourd'hui unanimement reconnu comme une forme d'expression universelle, son développement international fut souvent semé d'embûches. Il y a un siècle, aux Etats-Unis, le contexte politique et social bâillonnait les manifestations d'une culture «nègre» jugée primitive et sans valeur. Au même moment, la France coloniale portait un regard paternaliste et condescendant sur les populations noires d'Afrique. Ainsi, lorsque les premiers échos d'un jazz américain balbutiant parvinrent aux oreilles de musiciens européens aguerris, les réactions furent dubitatives, amusées, parfois dédaigneuses et toujours dictées par une compréhension primaire d'un art dit «exotique». Il est donc difficile de dater précisément la naissance du jazz en France. D'autant plus difficile d'en dessiner les contours... Le pianiste, chef d'orchestre et pédagogue français, Laurent Cugny, s'est lancé dans une étude approfondie du paysage musical européen en éditant le 1er Tome d'une série de 4 ouvrages, intitulée «Une histoire du Jazz en France» (Outre Mesure Editions), qui narre dans le détail l'évolution progressive des mentalités face à l'émergence d'une culture métisse, imprévisible et libre.</p>
+			<p>Avant 1808, la traite des esclaves avait déjà provoqué l'arrivée aux États-Unis d'un million d'Africains subsahariens, principalement en provenance d'Afrique de l'Ouest et du bassin du fleuve Congo. Ceux-ci ont importé leurs traditions musicales : la musique africaine utilisait une seule mélodie, des rythmes contre-métriques, et une structure en appel et réponse  (exemple : work song). Elle avait essentiellement un but fonctionnel, pour accompagner le travail (chant de travail) ou les rites funéraires. Des rassemblements d'esclaves donnaient lieu à de grandes fêtes urbaines où se mêlaient danses africaines et percussions. Jusqu'en 1843, une fête était organisée chaque dimanche à Congo Square, à la Nouvelle-Orléans, et d'autres rassemblements avaient lieu à la même époque dans le Sud des États-Unis.
 
 				En outre, le jazz a aussi pour origine la musique religieuse : les esclaves apprennent les harmonies des hymnes lors de l'office dominical, et y ajoutent des influences africaines pour créer les negro spirituals et le gospel, peu à peu chantés dans les églises méthodistes, baptistes ou pentecôtistes28. De même, au cours du xixe siècle, un nombre grandissant de musiciens noirs apprennent à jouer d'un instrument « européen », notamment le violon, et parodient la musique de bal dans les cakewalks. À l'inverse, les minstrel shows, réalisés par des Euro-Américains au visage peint en noir (blackface), combinent la syncope des rythmes africains et l'harmonie de la musique européenne.
 			</p>
 		</div>
+		
 
 		<div class="style">
-			<img src="https://www.thebridge2017.com/medias/00/00/60/5-nouveaux-artistes-jazz-a-decouvrir-de-toute-urgence-c-1920-650.jpg">
+			<img src="https://www.thebridge2017.com/medias/00/00/60/5-nouveaux-artistes-jazz-a-decouvrir-de-toute-urgence-c-1920-650.jpg" alt="jazz">
 
 			<h2>Ragtime et blues (1890-1910)</h2>
 
@@ -57,23 +54,23 @@
 		</div>
 
 		<div class="style">
-			<img src="http://static-cdn.arte.tv/cdnp-cinema/sites/default/files/styles/cine_rectangle_1240/public/thumbnails/image/jazztaenzer.png?itok=9luGAJgi">
+			<img src="http://static-cdn.arte.tv/cdnp-cinema/sites/default/files/styles/cine_rectangle_1240/public/thumbnails/image/jazztaenzer.png?itok=9luGAJgi" alt="jazz">
 
 			<h2>Jazz Nouvelle-Orléans et Dixieland (1910-1930)</h2>
 
-			<p>C'est à La Nouvelle-Orléans que l'on fait en général naître le jazz, avec les formations orchestrales des « brass bands », mélange de marches militaires revisitées par les noirs américains et les créoles, qui privilégie l'expression collective. Dans les années 1910 apparaissent les premières formes de jazz (« proto-jazz »), notamment sous l'impulsion du chef d'orchestre James Reese Europe, qui créa le Clef Club. Cette salle de concert de Harlem accueille dès 1912 le premier orchestre de jazz composé uniquement d'Afro-Américains, le Clef Club Orchestra. En 1913 et 1914 sont réalisés des enregistrements au phonographe pour la Victor Talking Machine Company.</br>
+			<p>C'est à La Nouvelle-Orléans que l'on fait en général naître le jazz, avec les formations orchestrales des « brass bands », mélange de marches militaires revisitées par les noirs américains et les créoles, qui privilégie l'expression collective. Dans les années 1910 apparaissent les premières formes de jazz (« proto-jazz »), notamment sous l'impulsion du chef d'orchestre James Reese Europe, qui créa le Clef Club. Cette salle de concert de Harlem accueille dès 1912 le premier orchestre de jazz composé uniquement d'Afro-Américains, le Clef Club Orchestra. En 1913 et 1914 sont réalisés des enregistrements au phonographe pour la Victor Talking Machine Company.<br />
 
-			Le premier enregistrement de jazz voit le jour en mars 1917 par l'Original Dixieland Jass Band. Autoproclamé inventeur du jazz, Jelly Roll Morton est en effet un passeur entre ragtime et jazz, mais ce sont Kid Ory, Sidney Bechet et surtout Louis Armstrong qui s'imposent comme les grands solistes des formations Nouvelle-Orléans caractérisées par l'improvisation collective sur le schéma instrumental trompette, trombone, clarinette.</p>
-			
+				Le premier enregistrement de jazz voit le jour en mars 1917 par l'Original Dixieland Jass Band. Autoproclamé inventeur du jazz, Jelly Roll Morton est en effet un passeur entre ragtime et jazz, mais ce sont Kid Ory, Sidney Bechet et surtout Louis Armstrong qui s'imposent comme les grands solistes des formations Nouvelle-Orléans caractérisées par l'improvisation collective sur le schéma instrumental trompette, trombone, clarinette.
+			</p>
 		</div>
 
 
 		<div class="style">
-			<img src="http://www.ombres-blanches.fr/uploads/tx_tableslibraires/jazz.jpg">
+			<img src="http://www.ombres-blanches.fr/uploads/tx_tableslibraires/jazz.jpg" alt="jazz">
 
 			<h2>Du swing au bebop</h2>
 
-			<p>Considéré comme l'âge d'or du jazz, apparu vers les années 1930, le swing (ou middle jazz) se démarque du jazz Nouvelle-Orléans par un orchestre de plus grande taille sur le modèle des trois sections de trompettes, trombones et anches qui privilégie les solistes prenant des chorus intégrés dans des arrangements écrits au détriment de l'improvisation collective. C'est l'ère des big bands de Duke Ellington, Count Basie, Glenn Miller, Benny Goodman, avec un répertoire marqué par les compositions de George Gershwin, Cole Porter, Richard Rodgers etc. et les chansons de variété de Tin Pan Alley, qui forment l'ossature des standards de jazz. Les grands solistes de cette époque sont Coleman Hawkins, Roy Eldridge, Benny Carter, Johnny Hodges, Ben Webster, Art Tatum, et Lester Young. </br>
+			<p>Considéré comme l'âge d'or du jazz, apparu vers les années 1930, le swing (ou middle jazz) se démarque du jazz Nouvelle-Orléans par un orchestre de plus grande taille sur le modèle des trois sections de trompettes, trombones et anches qui privilégie les solistes prenant des chorus intégrés dans des arrangements écrits au détriment de l'improvisation collective. C'est l'ère des big bands de Duke Ellington, Count Basie, Glenn Miller, Benny Goodman, avec un répertoire marqué par les compositions de George Gershwin, Cole Porter, Richard Rodgers etc. et les chansons de variété de Tin Pan Alley, qui forment l'ossature des standards de jazz. Les grands solistes de cette époque sont Coleman Hawkins, Roy Eldridge, Benny Carter, Johnny Hodges, Ben Webster, Art Tatum, et Lester Young. <br />
 
 				Au début des années 1940 naît le bebop. Tempos ultras rapides, petites formations, virtuosité époustouflante, innovations harmoniques et rythmiques, la rupture est brutale et emmenée par Charlie Parker, Dizzy Gillespie, Kenny Clarke, Thelonious Monk. L'intellectualisation du jazz par le bebop ne sera pas toujours bien perçue par le public et certains critiques, notamment Hugues Panassié en France sera particulièrement virulent contre cette nouvelle forme de jazz.
 
@@ -82,28 +79,29 @@
 
 
 		<div class="style">
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/1200px-Louis_Armstrong_restored.jpg">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/1200px-Louis_Armstrong_restored.jpg" alt="jazz">
 
 			<h2>BE BOP</h2>
 
-			<p>Considéré comme l'âge d'or du jazz, apparu vers les années 1930, le swing (ou middle jazz) se démarque du jazz Nouvelle-Orléans par un orchestre de plus grande taille sur le modèle des trois sections de trompettes, trombones et anches qui privilégie les solistes prenant des chorus intégrés dans des arrangements écrits au détriment de l'improvisation collective. C'est l'ère des big bands de Duke Ellington, Count Basie, Glenn Miller, Benny Goodman, avec un répertoire marqué par les compositions de George Gershwin, Cole Porter, Richard Rodgers etc. et les chansons de variété de Tin Pan Alley, qui forment l'ossature des standards de jazz. Les grands solistes de cette époque sont Coleman Hawkins, Roy Eldridge, Benny Carter, Johnny Hodges, Ben Webster, Art Tatum, et Lester Young.</br>
+			<p>Considéré comme l'âge d'or du jazz, apparu vers les années 1930, le swing (ou middle jazz) se démarque du jazz Nouvelle-Orléans par un orchestre de plus grande taille sur le modèle des trois sections de trompettes, trombones et anches qui privilégie les solistes prenant des chorus intégrés dans des arrangements écrits au détriment de l'improvisation collective. C'est l'ère des big bands de Duke Ellington, Count Basie, Glenn Miller, Benny Goodman, avec un répertoire marqué par les compositions de George Gershwin, Cole Porter, Richard Rodgers etc. et les chansons de variété de Tin Pan Alley, qui forment l'ossature des standards de jazz. Les grands solistes de cette époque sont Coleman Hawkins, Roy Eldridge, Benny Carter, Johnny Hodges, Ben Webster, Art Tatum, et Lester Young.<br />
 
 			Au début des années 1940 naît le bebop. Tempos ultras rapides, petites formations, virtuosité époustouflante, innovations harmoniques et rythmiques, la rupture est brutale et emmenée par Charlie Parker, Dizzy Gillespie, Kenny Clarke, Thelonious Monk. L'intellectualisation du jazz par le bebop ne sera pas toujours bien perçue par le public et certains critiques, notamment Hugues Panassié en France sera particulièrement virulent contre cette nouvelle forme de jazz.</p>
 		</div>
 
 		<div class="style">
-			<img src="http://www.musikzeitung.ch/.imaging/stk/musikzeitung/article-image-intro/website/fr/resonance/jazz/2017/Femmes-AMR/imageBinary/Mary_Lou_Williams_(Gottlieb_09231)_-_Crop.jpg">
+			<img src="http://www.musikzeitung.ch/.imaging/stk/musikzeitung/article-image-intro/website/fr/resonance/jazz/2017/Femmes-AMR/imageBinary/Mary_Lou_Williams_(Gottlieb_09231)_-_Crop.jpg" alt="jazz">
 
 			<h2>Cool jazz, hard bop, jazz modal, free jazz (années 1950)</h2>
 
 			<p>Le cool jazz né en 1949 sous la houlette de Miles Davis qui cherche à briser la spirale frénétique du be-bop vers plus de virtuosité. L'album The Birth of Cool en 1949 marque un retour à des rythmes plus apaisés et des compositions plus simples. Ce caractère plus abordable lui permettra de connaitre un succès rapide, en particulier auprès des musiciens de la côte ouest des États-Unis avant de se prêter à de nombreux métissages (notamment avec la musique brésilienne pour créer la bossa-nova ). Le cool jazz est un jazz libre, rempli de silence et d'intensité. Il contient tout le bebop dans ses phrasés, dans l'attitude des musiciens et c'est aussi une musique qui s'écoute facilement, que l'on peut apprécier dans tous les instants.<br />
 			Musiciens : Miles Davis - Chet Baker - Dave Brubeck  </p>
 		</div>
-
-		
-		<?php include ("includes/footer.php"); ?>
+	</div>
 
 
-	</body>
-	</html>
+	<?php include ("includes/footer.php"); ?>
+
+
+</body>
+</html>
 
